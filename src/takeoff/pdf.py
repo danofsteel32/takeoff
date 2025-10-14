0bda2d3b7a6076ddd3d8a4133f86a56cdd58eab2
danofsteel32 <dan@dandavis.dev>
2025-10-13 21:17:14 -0400

    ProcessedPDF type

--- src/takeoff/pdf.py
+++ src/takeoff/pdf.py
@@ -1,16 +1,60 @@
+import hashlib
 import logging
 from pathlib import Path
 
-# import fitz  # PyMuPDF
 import pymupdf as fitz  # type: ignore[import-untyped]
+from attrs import define
 
 DPI = 150
 
 logger = logging.getLogger(__name__)
 
 
-def process(path: Path) -> list[dict]:
-    logger.debug("BEGIN processing %s ...", path.name)
+@define
+class BBox:
+    x0: float
+    y0: float
+    x1: float
+    y1: float
+
+
+@define
+class TextBlock:
+    text: str
+    bbox: BBox
+
+
+@define
+class ProcessedPage:
+    page_number: int
+    width: int
+    height: int
+    zoom_factor: float
+    raw_text: str
+    page_image: fitz.Pixmap
+    text_blocks: list[TextBlock]
+
+
+@define
+class ProcessedPDF:
+    filename: str
+    sha256sum: str
+    pages: list[ProcessedPage]
+
+
+def get_sha256sum(path: Path, chunk_size: int = 65536) -> str:
+    hasher = hashlib.sha256()
+    with open(path, "rb") as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if not chunk:
+                break
+            hasher.update(chunk)
+    return hasher.hexdigest()
+
+
+def process(path: Path) -> ProcessedPDF:
+    logger.debug("Begin processing %s ...", path.name)
 
     doc = fitz.open(path)
 
@@ -20,51 +64,56 @@ def process(path: Path) -> list[dict]:
 
     for n in range(doc.page_count):
         page_num = n + 1
+
         logger.debug("Processing page %d/%d in %s", page_num, doc.page_count, doc.name)
         page = doc[n]
-        page_data: dict = {}
-        page_data["page_number"] = page_num
-        page_data["page_width"] = page.rect.width
-        page_data["page_height"] = page.rect.height
 
         logger.debug("Extracting text")
-
-        text = page.get_text("text")
-        page_data["full_text"] = text
-
-        logger.info(
-            "Extracted %d chars from page %d in %s", len(text), page_num, doc.name
+        raw_text = page.get_text("text")
+        logger.debug(
+            "Extracted %d chars from page %d in %s", len(raw_text), page_num, doc.name
         )
 
         logger.debug("Extracting text blocks")
-
-        text_blocks_with_bboxes = []
         dict_data = page.get_text("dict")
         blocks = dict_data.get("blocks", [])
-
         logger.debug("Found %d blocks", len(blocks))
 
+        text_blocks = []
         # blocks -> lines -> spans
         for block in blocks:
             # Block type 0 is text, type 1 is image
             if block["type"] == 0 and "lines" in block:
                 for line in block.get("lines", []):
                     for span in line.get("spans"):
-                        text_blocks_with_bboxes.append(
-                            {"text": span["text"], "bbox": list(span["bbox"])}
+                        text_block = TextBlock(
+                            text=span["text"], bbox=BBox(**span["bbox"])
                         )
-                        logger.debug("Text block '%s'", span["text"])
-
-        page_data["text_blocks"] = text_blocks_with_bboxes
+                        logger.debug("TextBlock('%s')", span["text"])
+                        text_blocks.append(text_block)
 
         zoom_factor = DPI / 72.0  # 72 is the default DPI for PDF points
         mat = fitz.Matrix(zoom_factor, zoom_factor)
-        pix = page.get_pixmap(matrix=mat, alpha=False)  # alpha=False for JPEG
-        # TODO actually do something with the image
-        page_data["image"] = str(pix)
-        page_data["zoom_factor"] = zoom_factor
+        pix = page.get_pixmap(matrix=mat)
+        logger.debug(
+            "Pixmap(width=%d, height=%d, size=%d)", pix.width, pix.height, pix.size
+        )
 
-        pages.append(page_data)
+        processed_page = ProcessedPage(
+            page_number=page_num,
+            width=page.rect.width,
+            height=page.rect.height,
+            zoom_factor=zoom_factor,
+            raw_text=raw_text,
+            page_image=pix,
+            text_blocks=text_blocks,
+        )
+        pages.append(processed_page)
+        logger.debug("Processed page %d/%d in %s", page_num, doc.page_count, doc.name)
 
     doc.close()
-    return pages
+
+    sha256sum = get_sha256sum(path)
+    processed_pdf = ProcessedPDF(filename=path.name, sha256sum=sha256sum, pages=pages)
+    logger.info("Processed PDF %s", processed_pdf.filename)
+    return processed_pdf
